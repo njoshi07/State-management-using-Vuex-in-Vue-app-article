4d49fb2d6d65ce2de9a1729e278f56a4f6564101
Nishit <nishitdjoshi@gmail.com>
2023-02-20 01:27:22 -0600

    test added for mutations

--- tests/unit/task.spec.js
+++ tests/unit/task.spec.js
@@ -1,13 +1,40 @@
-import { shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Task from "@/components/Task.vue";
 
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
 describe("tests for Task ", () => {
+  let store;
+  let state;
+  let mutations;
+
+  // **** Mocking process **** //
+  beforeEach(() => {
+    state = {
+      TasksLists: [
+        { id: 1, title: "task 1", completed: false },
+        { id: 1, title: "task 1", completed: false },
+      ],
+    };
+    mutations = {
+      pullTasks: jest.fn(),
+
+      DeleteTask: jest.fn(),
+    };
+
+    store = new Vuex.Store({ mutations });
+  });
+
+  // *****
+
   it("does compoent has task", async () => {
     const wrapper = shallowMount(Task);
     expect(wrapper.classes("taskDetail")).toBe(true);
   });
 
-  //   Props Testing
+  // Testing Props
   it("testing props value", () => {
     const wrapper = shallowMount(Task, {
       propsData: {
@@ -18,5 +45,17 @@ describe("tests for Task ", () => {
 
     expect(wrapper.props().taskDesc).toBe("bazz");
     expect(wrapper.props().index).toBe(1);
+  });
+
+  // Testing Mutation
+
+  it("delete task when you click on delete button", async () => {
+    const wrapper = shallowMount(Task, {
+      store,
+      localVue,
+    });
+
+    await wrapper.find("button").trigger("click");
+    expect(mutations.DeleteTask).toBeCalled();
   });
 });
